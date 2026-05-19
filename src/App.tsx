de39e6e2a2e970a7d0a47b34ddca3dd842778b16
Gregory Maccio <gregorymaccio@blocked.local>
2026-05-19 16:32:03 +0200

    Revert "feat: add license expiry joke modal on startup"
    This reverts commit 6126a10d3838d988bd7a65b57e082fabdeb5df0f.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Toolbar from './components/Toolbar/Toolbar'
 import CanvasArea from './components/Canvas/CanvasArea'
 import RightPanel from './components/RightPanel/RightPanel'
 import CalibrationModal from './components/Modals/CalibrationModal'
-import LicenseModal from './components/Modals/LicenseModal'
 import { useProjectStore } from './store/useProjectStore'
 import { saveProject } from './lib/projectStorage'
 import { usePdfStore } from './store/usePdfStore'
@@ -42,7 +41,6 @@ function App() {
         <RightPanel />
       </div>
       <CalibrationModal />
-      <LicenseModal />
     </div>
   )
 }
